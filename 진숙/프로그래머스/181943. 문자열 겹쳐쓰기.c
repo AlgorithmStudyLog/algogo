a7e5660c77dc4bdec05d56475524913b2cccc83d
jinsukuku <jinsukuku@gmail.com>
2025-04-06 23:31:21 +0900

    Slove: 스터디 5회차 사전 문제 풀이 완료

--- 진숙/프로그래머스/181943. 문자열 겹쳐쓰기.c
+++ 진숙/프로그래머스/181943. 문자열 겹쳐쓰기.c
@@ -6,7 +6,7 @@
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 char* solution(const char* my_string, const char* overwrite_string, int s) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    int len = strlen(my_string) + 1;
+    int len = strlen(my_string) + 1; // 문자열 길이는 Null 제외한 값이므로 +1 필요
     char* answer = (char*)malloc(len * sizeof(char));
     strcpy(answer, my_string);
     

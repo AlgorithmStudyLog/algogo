2c259279c1c083e411977c9c14f78b99a113051f
seesee1212 <suzumya@naver.com>
2025-04-09 15:31:41 +0900

    Wip : 10809

---
+++ 승우/백준/브론즈/10809_알파벳 찾기.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+	int i;
+	int aa[26];
+	char AA[101];
+
+	scanf("%s", AA);
+
+	for (i = 0; i < 26; i++)
+	{
+		aa[i] = -1;
+	}
+
+	for (i = 0; ; i++)
+	{
+
+	}
+
+	for (i = 0; i < 26; i++)
+	{
+		printf("%d ", aa[i]);
+	}
+	
+	return 0;
+}

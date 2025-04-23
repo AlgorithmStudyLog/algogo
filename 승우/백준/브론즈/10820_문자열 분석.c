4221525547d95e349e0b8133360844f4c8878f9f
seesee1212 <suzumya@naver.com>
2025-04-23 12:47:56 +0900

    WIP : 10820

--- 승우/백준/브론즈/10820_문자열 분석.c
+++ 승우/백준/브론즈/10820_문자열 분석.c
@@ -3,11 +3,11 @@
 int main() {
     char line[101]; 
 
-    while (fgets(line, 101, stdin)) 
+    while (fgets(line, 101, stdin)) //문자열을 입력받을 떄 사용 (배열, 크기, 입력)
     { 
         int small = 0, big = 0, num = 0, space = 0;
 
-        for (int i = 0; line[i] != '\0'; i++) 
+        for (int i = 0; line[i] != '\0'; i++) // 문자열이 끝날때까지 확인
         {
             char c = line[i];
 

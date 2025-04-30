b9a58e34a930e40779696b37dba9f130d46a5770
jinsukuku <jinsukuku@gmail.com>
2025-04-30 16:54:56 +0900

    WIP: 1465. 방번호

---
+++ 진숙/백준/실버/1475. 방번호.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int arr[9] = {0};   // 사용된 숫자 확인하기위한 배열 (0~8) , 9인 경우 입력된 경우 6을+1한다
+    char num[10];       // 1,000,000보다 작거나 같은 자연수
+    scanf("%s", num);
+
+
+    int len = strlen(num);
+    for(int i = 0; i < len; i++){
+        printf("%c", num[i]);
+        int idx = num[i] - '0';
+
+        if(idx == 9 || idx == 6) arr[6]++;
+        else arr[idx]++;
+    }
+
+    int maxCnt = 0;
+    if(arr[6] % 2 == 0) arr[6] = arr[6] / 2;
+    else arr[6] = arr[6] / 2 + 1;  // 홀수일 때는 올림 처리
+    for(int i = 0; i < 9; i++){
+        if(maxCnt < arr[i]) maxCnt = arr[i];
+    }
+
+    printf("%d", maxCnt);
+
+    return 0;
+}

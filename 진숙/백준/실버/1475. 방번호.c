c1946a5573afb09535969a17239d5ad24f085786
jinsukuku <jinsukuku@gmail.com>
2025-05-07 00:51:41 +0900

    Slove: 백준 1475. 방번호

--- 진숙/백준/실버/1475. 방번호.c
+++ 진숙/백준/실버/1475. 방번호.c
@@ -5,22 +5,22 @@ int main(){
     int arr[9] = {0};   // 사용된 숫자 확인하기위한 배열 (0~8) , 9인 경우 입력된 경우 6을+1한다
     char num[10];       // 1,000,000보다 작거나 같은 자연수
     scanf("%s", num);
-
+    getchar();
 
     int len = strlen(num);
     for(int i = 0; i < len; i++){
-        printf("%c", num[i]);
         int idx = num[i] - '0';
-
         if(idx == 9 || idx == 6) arr[6]++;
         else arr[idx]++;
     }
 
     int maxCnt = 0;
-    if(arr[6] % 2 == 0) arr[6] = arr[6] / 2;
-    else arr[6] = arr[6] / 2 + 1;  // 홀수일 때는 올림 처리
+    if(arr[6] % 2 != 0) arr[6]++;
+    arr[6] /= 2;
     for(int i = 0; i < 9; i++){
-        if(maxCnt < arr[i]) maxCnt = arr[i];
+        if(maxCnt < arr[i]){
+            maxCnt = arr[i];
+        }
     }
 
     printf("%d", maxCnt);

e9c126edb80249f0f9c1b20f85ede23de832b806
morningB <yjw001205@gmail.com>
2025-06-13 14:15:45 +0900

    Solve : week14 1991 문제 해결

--- 주원/백준/실버/1991. 트리 순회.cpp
+++ 주원/백준/실버/1991. 트리 순회.cpp
@@ -3,14 +3,31 @@
 #include <vector>
 
 using namespace std;
-#define MAX 100001
 
-vector<int> arr[MAX];
-int value[MAX];
-bool visited[MAX];
+int tree[26][2];
 
-void dfs(int root);
-void bfs(int root);
+
+void preorder(char node)
+{
+    if(node == -1) return;
+    cout << (char)(node + 'A');
+    preorder(tree[node][0]);
+    preorder(tree[node][1]);
+}
+void inorder(char node)
+{
+    if(node == -1) return;
+    inorder(tree[node][0]);
+    cout << (char)(node + 'A');
+    inorder(tree[node][1]);
+}
+void postorder(char node)
+{
+    if(node == -1) return;
+    postorder(tree[node][0]);
+    postorder(tree[node][1]);
+    cout << (char)(node + 'A');
+}
 
 int main() {
     ios_base::sync_with_stdio(false); // C++ 스트림과 C 스트림의 동기화 비활성화
@@ -18,58 +35,29 @@ int main() {
 
     int n;
     cin >> n; // 노드의 개수
-    for(int i=0;i<n - 1;i++) // 간선의 개수
+    for(int i=0;i<n;i++) // 간선의 개수
     {
-        int a,b;
-        cin >> a>>b;
-
-        // 서로 이어줌 why? -> 양방향 그래프이기 때문에
-        arr[a].push_back(b); 
-        arr[b].push_back(a);
-    }
-
-    dfs(1);
-    for(int i =2;i<=n;i++)
-        cout << value[i] << "\n";
-    
-}
-
-// DFS 버전
-void dfs(int root)
-{
-    visited[root] = true;
-    for(int i=0;i<arr[root].size();i++)
-    {
-        int w = arr[root][i];
-        if(!visited[w])
-        {
-            value[w] = root;
-            dfs(w);
-        }
-    }
-}
-
-// BFS 버전
-void bfs(int root)
-{
-    queue<int> q;
-    q.push(root);
-    visited[root] = true;
-
-    while(!q.empty())
-    {
-        int x = q.front();
-        q.pop();
+        // 각 노드와 왼쪽 자식, 오른쪽ㅈ ㅏ식이 주어짐.
+        // 이름은 문자열이고, A가 root, 자식 없다면 . 
+        char node,left,right;
+        cin >> node >> left >> right;
+        node -= 'A';
         
-        for(int i=0; i<arr[x].size();i++)
-        {
-            int y = arr[x][i];
-            if(!visited[arr[x][i]])
-            {
-                value[arr[x][i]] = x;
-                q.push(arr[x][i]);
-                visited[arr[x][i]] = true;
-            }
-        }
+        if(left == '.')
+            tree[node][0] = -1;
+        else    
+            tree[node][0] = left - 'A';
+        if(right == '.')
+            tree[node][1] = -1;
+        else
+            tree[node][1] = right - 'A';
+                
     }
+
+    preorder(0); // 전위
+    cout << endl;
+    inorder(0); // 중위
+    cout << endl; 
+    postorder(0); // 후위
+    cout << endl;
 }

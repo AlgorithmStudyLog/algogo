2fca4910a58cb755c3866414924db8cfb5291a58
Joowon_Yun <yjw001205@gmail.com>
2025-07-21 12:35:56 +0900

    Solve: 2217 로프 문제 해결

--- 주원/백준/실버/2217.로프.cpp
+++ 주원/백준/실버/2217.로프.cpp
@@ -8,12 +8,10 @@ using namespace std;
 
 int main()
 {
-	int n = 0, min_loop = 10001;
+	int n = 0, answer = 0;
 	cin >> n;
 	vector<int> arr;
-	int re1 = 0, re2 = 10001;
-	int sum = 0;
-
+	
 	for (int i = 0;i < n;i++)
 	{
 		int loop;
@@ -22,33 +20,14 @@ int main()
 	}
 
 	sort(arr.begin(), arr.end());
+	reverse(arr.begin(), arr.end());
 
 	for (int i = 0;i < arr.size();i++)
 	{
-		min_loop = min(min_loop, arr[i]);
-	}
+		int temp = arr[i] * (i+1);
 
-	re1 = min_loop * arr.size();
-
-	for (int i = 0;i < arr.size();i++)
-	{
-		sum += arr[i];
+		if (temp > answer)
+			answer = temp;
 	}
-
-	int avg = sum / arr.size();
-	int cnt = 0;
-	
-	
-	for (int i = 0;i < arr.size();i++)
-	{
-		if (avg < arr[i])
-		{
-			cnt++;
-			re2 = min(arr[i], re2);
-		}
-		
-	}
-	
-	re2 = cnt * re2;
-	cout << max(re1, re2);
+	cout << answer;
 }

1be54eef04d1abf779fe967aa74a0d69699079c4
Lazylionr6 <syeop98@gmail.com>
2025-08-04 12:03:22 +0900

    WIP : 백준 11052

---
+++ 상엽/백준/실버/알고리즘_003.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int cost(int N, const vector<int>& P, vector<double>& I);
+
+int main()
+{
+	int N, i;
+	int n;
+
+	cin >> N;
+
+	vector<int> P(N + 1);
+	vector<double> I(N + 1);
+
+	for (i = 1; i <= N; i++)
+	{
+		cin >> P[i];
+		I[i] = P[i] / i;
+	}
+
+	n = cost(N, P, I);
+
+	cout << n << endl;
+
+	return 0;
+}
+
+int cost(int N, const vector<int>& P,const vector<double>& I)
+{
+	if (N != 0)
+	{
+		double max = -1;
+		int maxP;
+		int i, num;
+
+		for (i = 1; i <= N; i++)
+		{
+			if (I[i] > max)
+			{
+				maxP = i;
+				max = I[i];
+			}
+		}
+		int M = N % maxP;
+		return num = P[maxP] * (N / maxP) + cost(M, P, I);
+	}
+	else return 0;
+}
